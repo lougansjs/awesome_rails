6049fcd25fc1c7a4017beb5df5f69d6ed27bbc49
LougansJs <lougans.lougans99@gmail.com>
2021-06-13 13:35:40 -0300

    :art: Criando login e registro de usuário

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   include ForgeryProtection
   include SetPlatform
+
+  def after_sign_out_path_for(resource_or_scope)
+    user_session_path
+  end
 end
