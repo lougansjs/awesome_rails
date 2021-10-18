7c075a986ba0d767f4aaa15d96004304b610fb40
Lougans de Matos <lougans.lougans99@gmail.com>
2021-10-18 03:03:38 -0300

    :rocket: Finalizando burndown

--- app/controllers/burndowns_controller.rb
+++ app/controllers/burndowns_controller.rb
@@ -5,8 +5,14 @@ class BurndownsController < ApplicationController
   before_action :authenticate_user!
   def index
     burndown = Burndown.where("'#{Date.today}' between date_start AND date_end").first
-    score = burndown.score_by_day
-    @series = [{ name: 'Planejado', data: score }]
+    planned = burndown.planned_by_day
+    activities = artia_svc.count_activities(burndown.mapping_passed_days)
+    executed = burndown.executed_by_day(activities)
+    @series = [{ name: 'Planejado', data: planned }, { name: 'Realizado', data: executed }]
     @options = { title: "SPRINT #{burndown.sprint}", theme: 'rainbow' }
+  end
+
+  def artia_svc
+    @artia_svc = ArtiaService.new
   end
 end
