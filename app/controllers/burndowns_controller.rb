a26b60c80c3b68acc8d1b4896abc24895ff75574
Lougans de Matos <lougans.lougans99@gmail.com>
2021-10-18 23:29:33 -0300

    :rocket: Mudanças no burndown

--- app/controllers/burndowns_controller.rb
+++ app/controllers/burndowns_controller.rb
@@ -4,12 +4,12 @@
 class BurndownsController < ApplicationController
   before_action :authenticate_user!
   def index
-    burndown = Burndown.where("'#{Date.today}' between date_start AND date_end").first
-    planned = burndown.planned_by_day
-    activities = artia_svc.count_activities(burndown.mapping_passed_days)
-    executed = burndown.executed_by_day(activities)
+    @burndown = Burndown.where("'#{Date.today}' between date_start AND date_end").first
+    planned = @burndown.planned_by_day
+    activities = artia_svc.count_activities(@burndown.mapping_passed_days)
+    executed = @burndown.executed_by_day(activities)
     @series = [{ name: 'Planejado', data: planned }, { name: 'Realizado', data: executed }]
-    @options = { title: "SPRINT #{burndown.sprint}", theme: 'rainbow' }
+    @options = { title: "SPRINT #{@burndown.sprint}", theme: 'rainbow' }
   end
 
   def artia_svc
